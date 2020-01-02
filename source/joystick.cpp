beebe44cc3d10a4095536b5a4fb2a0ca398139fb
I am TSB <retro@tsb.space>
2020-01-02 09:57:07 +0100

    change license info

--- source/joystick.cpp
+++ source/joystick.cpp
@@ -1,16 +1,35 @@
 /*
- * joystick.cpp
- * 
- * Joystick routines to handle arcade to usb conversion
- * 
- * Version 1.1
- * Release date: 2019-04-19
- * 
- * Written & released by M.F. Wieland (TSB)
- * 
- * This is free and unencumbered software released into the public domain.
- * See the file LICENSE for more details, or visit <http://unlicense.org>.
- */
+joystick.cpp
+ 
+Joystick routines to handle arcade to usb conversion
+ 
+Version 1.1
+Release date: 2019-04-19
+
+Written & released by M.F. Wieland (TSB)
+
+MIT License
+
+Copyright (c) 2019 M.F. Wieland (TSB)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
 
 
 #include "joystick.h"
